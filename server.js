3de71ba3e438e71a451a8695bacf90c35361da79
BlockslideCreator <jerrycruncher100@gmail.com>
2026-02-28 16:42:20 -0600

    Update server.js

--- server.js
+++ server.js
@@ -248,4 +248,9 @@ function startTimer(code) {
     }, 1000);
 }
 
-server.listen(3000, () => console.log("Blockslide running on http://localhost:3000"));
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log("Blockslide running on port " + PORT);
+});
